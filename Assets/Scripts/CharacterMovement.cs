772597711f099ba12d193b4811d6fe1b70efafa6
Caaakey <kbop2000@gmail.com>
2020-04-05 17:53:20 +0900

    Update Animation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,14 +4,48 @@ public class CharacterMovement : MonoBehaviour
 {
     public Animator animator = null;
     public Rigidbody2D rigid = null;
+    public CircleCollider2D bottomCollider = null;
     public float speed = 1.5f;
-    public float jumpPower = 10f;
+    public float jumpPower = 5f;
+    public AnimationClip attackClip = null;
+    public AnimationClip airAttackClip = null;
 
     private bool isMove = false;
     private bool isReverse = false;
-
+    private bool isDontMove = false;
+    
     private void Update()
     {
+        OnAttackControll();
+        OnMoveControll();
+    }
+
+    private void OnAttackControll()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("Attack");
+
+            if (animator.GetBool("isJump") || animator.GetBool("isJumpDown"))
+            {
+                CancelInvoke("OnMoveContinue");
+                Invoke("OnMoveContinue", airAttackClip.length);
+            }
+            else
+            {
+                isMove = false;
+                isDontMove = true;
+
+                CancelInvoke("OnMoveContinue");
+                Invoke("OnMoveContinue", attackClip.length);
+            }
+        }
+    }
+
+    private void OnMoveControll()
+    {
+        if (isDontMove) return;
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             Move(false);
@@ -30,6 +64,7 @@ public class CharacterMovement : MonoBehaviour
             if (!animator.GetBool("isJump"))
             {
                 rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
                 animator.SetBool("isJump", true);
                 animator.SetBool("isJumpDown", false);
             }
@@ -43,13 +78,7 @@ public class CharacterMovement : MonoBehaviour
         if (isMove)
             transform.Translate(speed * Time.fixedDeltaTime, 0, 0);
 
-        if (animator.GetBool("isJump"))
-        {
-            if (rigid.velocity.y < 0)
-            {
-                animator.SetBool("isJumpDown", true);
-            }
-        }
+        Jumping();
     }
 
     private void Move(bool isLeft)
@@ -68,14 +97,33 @@ public class CharacterMovement : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void Jumping()
     {
-        animator.SetBool("isJump", false);
+        if (rigid.velocity.y == 0) return;
+        if (rigid.velocity.y <= -Mathf.Epsilon)
+        {
+            bottomCollider.enabled = true;
+            animator.SetBool("isJumpDown", true);
+            animator.SetBool("isMove", false);
+        }
     }
 
-    private void Jump()
+    private void OnTriggerStay2D(Collider2D collision)
     {
+        if (collision.CompareTag("Tile"))
+        {
+            animator.SetBool("isJump", false);
+            animator.SetBool("isJumpDown", false);
 
+            if (rigid.velocity.y == 0)
+                bottomCollider.enabled = false;
+        }
+    }
+
+    private void OnMoveContinue()
+    {
+        animator.ResetTrigger("Attack");
+        isDontMove = false;
     }
 
 }
